43f5d0f6edea023252f8e3d528cc9b307ce31019
Avaer Kazmer <a@modules.io>
2019-11-15 05:38:23 -0500

    Add reifyHtml method

--- html-utils.js
+++ html-utils.js
@@ -41,7 +41,34 @@ function serializeHtml(j) {
   _recurse(j);
   return parse5.serialize(j);
 }
+function reifyHtml(j) {
+  const _recurse = n => {
+    const {nodeName} = n;
+    if (nodeName === '#document-fragment') {
+      const div = document.createElement('div');
+      for (let i = 0; i < n.childNodes.length; i++) {
+        const c = _recurse(n.childNodes[i]);
+        div.appendChild(c);
+      }
+      return div;
+    } else if (nodeName === '#text') {
+      return document.createTextNode(n.value);
+    } else {
+      const node = document.createElement(n.tagName);
+      for (const k in n.attrs) {
+        const {name, value} = n.attrs[k];
+        node.setAttribute(name, value);
+      }
+      for (let i = 0; i < n.childNodes.length; i++) {
+        node.appendChild(_recurse(n.childNodes[i]));
+      }
+      return node;
+    }
+  };
+  return _recurse(j);
+}
 export {
   parseHtml,
   serializeHtml,
+  reifyHtml,
 };
